7433bfe1909f94bedfc81996a3616928e583b8a6
Niudfear <hello@itcast.cn>
2024-10-30 13:51:35 +0800

    可以重复下子bug修改

--- src/main/java/org/example/game/entity/LocationEntity.java
+++ src/main/java/org/example/game/entity/LocationEntity.java
@@ -1,14 +1,17 @@
 package org.example.game.entity;
 
+import lombok.Data;
+
+@Data
 public class LocationEntity {
 
-    private int x;//某个棋盘位置横坐标，0-14
-    private int y;//某个棋盘位置纵坐标，0-14
+    //某个棋盘位置横坐标，0-14
+    private int x;
+    //某个棋盘位置纵坐标，0-14
+    private int y;
+    //占据该位置的棋手方，1是人类，-1是机器，0是空
+    private int owner;
 
-    private int owner;//占据该位置的棋手方，1是人类，-1是机器，0是空
-    private int score;//对该位置的打的分数
-
-    public LocationEntity(){}
     public LocationEntity(int x, int y, int owner){
         this.x = x;
         this.y = y;
@@ -21,12 +24,7 @@ public class LocationEntity {
      * @param y
      */
     public LocationEntity(int x, int y){
-        new LocationEntity(x,y,1);
-    }
-
-    public LocationEntity(int x, int y, int owner, int score){
-        this(x, y, owner);
-        this.score = score;
+        this(x,y,1);
     }
 
     public LocationEntity(LocationEntity location){
@@ -34,18 +32,5 @@ public class LocationEntity {
         this.y = location.y;
         this.owner = location.owner;
     }
-
-    public int getX(){return this.x;}
-    public void setX(int x){this.x = x;}
-
-    public int getY(){return this.y;}
-    public void setY(int y){this.y = y;}
-
-    public int getOwner(){return this.owner;}
-    public void setOwner(int owner){this.owner = owner;}
-
-    public int getScore(){return this.score;}
-    public void setScore(int score){this.score = score;}
-
 
 }
